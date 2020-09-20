fa499e2e4cecf5729d1645334f27e6e72133ea87
marvikfr <paris@outlook.fr>
2020-09-20 17:49:12 +0200

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Todo from './Todo'
+
+const TodoList = ({ tasks, match, onToggleCompleted }) => {
+  let filteredTasks
+
+  switch (match.params.filter) {
+    case 'completed' :
+      filteredTasks = tasks.filter(task => task.completed)
+      break
+    default:
+      filteredTasks = tasks
+  }
+
+  if (filteredTasks.length === 0) {
+    return (
+      <>
+        <h1 className='m-3'>Liste de tâches</h1>
+        <ul className='list-group m-3'>
+          <li className='list-group-item'>Aucune tâche à afficher</li>
+        </ul>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h1 className='m-3'>Liste de tâches</h1>
+        <ul className='list-group m-3'>
+          {
+            filteredTasks.map((task) => <Todo task={task} key={task.id} onToggleCompleted={onToggleCompleted} />)
+          }
+        </ul>
+      </>
+    )
+  }
+}
+
+export default TodoList
